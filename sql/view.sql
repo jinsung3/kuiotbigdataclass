80457464c64d84c8240ce05915c976d306bdf337
jinsung3 <js980902@gmail.com>
2024-03-07 12:30:24 +0900

    sadsad

--- sql/view.sql
+++ sql/view.sql
@@ -19,3 +19,21 @@ as select od.orderid, cs.custid, cs.name, bk.bookid,
 select orderid, bookname, saleprice
 from vw_Orders
 where name like '김연아';
+
+-- 4-22 example
+
+create view vw_Customer
+as select * from Customer where address like "%대한민국%";
+
+select * from vw_Customer;
+create or replace view vw_Customer(custid, name, address)
+as select custid, name, address
+	from Customer
+    where address like "%영국%";
+select * from vw_Customer;
+select * from Customer;
+
+drop view vw_Customer;
+insert into vw_Customer value (6, 최수길, "영굴 계룡");
+insert into Customer value (7, "박정석", "영국 대전", "000-0000-0101");
+
