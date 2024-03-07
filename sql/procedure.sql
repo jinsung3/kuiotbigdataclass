f17876c353a4bcd1717d97dd80e9efc83a15fd45
jinsung3 <js980902@gmail.com>
2024-03-07 16:18:46 +0900

    sads

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -1,4 +1,5 @@
 delimiter //;
+
 create procedure dorepeat2(p1 int)
 begin
 	set @x= 0;
@@ -12,5 +13,112 @@ delimiter;
 
 call dorepeat(100);
 select @x;
+USE `madangdb`;
+DROP procedure IF EXISTS `insertbook`;
 
---
+USE `madangdb`;
+DROP procedure IF EXISTS `madangdb`.`insertbook`;
+;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE  PROCEDURE `insertbook`(
+	in mybookid integer,
+    in mybookname varchar(40),
+    in mypublisher varchar(40),
+    in myprice integer)
+BEGIN
+	insert into Book(bookid, bookname, publisher, price)
+		values(mybookid, mybookname, mypublisher, myprice);
+END$$
+
+DELIMITER ;
+;
+-- proceddure registraion end
+
+call insertbook(13, '스포츠과학', '마당과학서적', 25000);
+select * from Book;
+USE `madangdb`;
+DROP procedure IF EXISTS `BookInsertOrUPDATE`;
+
+USE `madangdb`;
+DROP procedure IF EXISTS `madangdb`.`BookInsertOrUPDATE`;
+;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE  PROCEDURE `BookInsertOrUPDATE`(
+	in mybookid integer,
+    in mybookname varchar(40),
+    in mypublisher varchar(40),
+    in myprice integer)
+BEGIN
+	declare mycount integer;
+    select count(*) into mycount from Book
+		where bookname like mybookname;
+	if mycount != 0 then
+		set sql_safe_updates=0;
+        update Book set price = myprice
+			where bookname like mybookname;
+	else
+		insert into Book(bookid, bookname, publisher, price)
+        values(mybookid, mybookname, mypublisher, myprice);
+	end if;
+END$$
+
+DELIMITER ;
+
+-- BookInsertOrUpdate procedure resigtration end
+select * from Book;
+call BookInsertOrUpdate(15, '스포츠 즐거움', '마당과학서점', '25000');
+call BookInsertOrUpdate(16, '스포츠 즐거움', '마당과학서점', '30000');
+
+-- AveragePrice procedure resigtration end
+USE `madangdb`;
+DROP procedure IF EXISTS `AveragePrice`;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE PROCEDURE AveragePrice (
+	out AverageVal integer)
+BEGIN
+	select avg(price) into AverageVal
+    from Book where price is not null;
+END$$
+
+DELIMITER ;
+-- AveragePrice procedure resigtration end
+call averagePrice(@myprice);
+select @myprice;
+USE `madangdb`;
+DROP procedure IF EXISTS `Interest`;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE PROCEDURE Interest ()
+BEGIN
+	declare myInterest integer default 0.0;
+    declare price integer;
+    declare endOfRow boolean default false;
+    declare interestCursor cursor for select saleprice from Orders;
+    declare continue handler for not found set endOfRow=true;
+    open interestCursor;
+    cursor_loop : loop
+		fetch interestCursor into price;
+        if endOfRow then leave cursor_loop;
+        end if;
+        if price >= 30000 then
+			set myInterest = myInterest + price * 0.1;
+        else
+			set myInterest = myInterest + price * 0.05;
+        end if;
+	end loop cursor_loop;
+    close interestCursor;
+    select concat('전체이익금액 =', myInterest);
+END$$
+
+DELIMITER ;
+
+-- Interest procedure resigtration end
+
+call Interest();
---
+++ sql/trigger.sql
@@ -0,0 +1,50 @@
+set global log_bin_trust_function_creators=on;
+create table Book_log(
+	bookid integer primary key,
+    bookname varchar(40),
+    publisher varchar(40),
+    price integer);
+    
+-- book_after_insert trigger registration
+
+DROP TRIGGER IF EXISTS `madangdb`.`Book_AFTER_INSERT`;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `madangdb`.`Book_AFTER_INSERT` AFTER INSERT ON `Book` FOR EACH ROW
+BEGIN
+    insert into Book_log values (new.bookid, new.bookname, new.publisher, new.price);
+END$$
+DELIMITER ;
+-- book_after_insert trigger registration end
+
+select * from Book;
+select * from Book_log;
+insert into Book values (14, '스포츠 과학1', '이상미디어', 25000);
+
+-- fnc_interest registration end
+
+USE `madangdb`;
+DROP function IF EXISTS `fnc_interest`;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE FUNCTION fnc_interest(price integer)
+RETURNS INTEGER
+BEGIN
+	declare myInterest integer;
+    if price >= 30000 then
+		set myInterest = price * 0.1;
+    else
+		set myInterest = price * 0.05;
+    end if;
+RETURN myInterest;
+END$$
+
+DELIMITER ;
+
+-- fnc_interest registration end
+
+select custid, orderid, fnc_interest(saleprice) as 'interest'
+from Orders;
+
